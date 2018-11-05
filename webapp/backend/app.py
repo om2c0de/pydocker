1b2d022065f8932e58347acb01ffe3c358c34dd1
om2c0de <alexandr@raisesoftware.com>
2018-11-05 16:31:21 +0300

    Initial configuration: Flask App + Redis.

---
+++ webapp/backend/app.py
@@ -0,0 +1,29 @@
+import time
+import redis
+from flask import Flask
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port='6379')
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def homepage():
+    count = get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999, debug=True)
